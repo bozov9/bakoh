a464e48a038853ff6879145b066e35bc3bac17b9
Syamsul Arifin <arifsaqa@gmail.com>
2023-07-15 11:17:03 +0700

    fix: asset

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <Image  src={'/logo.jpeg'} alt='logo' height={80} width={80}/>
+      <Image  src={'/bakoh/logo.jpeg'} alt='logo' height={80} width={80}/>
     </Link>
   )
 }
